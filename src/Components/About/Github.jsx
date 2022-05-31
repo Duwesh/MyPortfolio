c98357ae296153ef801b4a38cf83d6f6449b4668
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-05-31 13:39:55 +0530

    added github calender with latest data

--- src/Components/About/Github.jsx
+++ src/Components/About/Github.jsx
@@ -10,7 +10,7 @@ export const Github = () => {
     grade3: "#b22ff4",
     grade2: "#b265f6",
     grade1: "#c084f5",
-    grade0: "#ecd9fc"
+    grade0: "#ecd9fc",
   };
   const [{ themename }] = React.useContext(ThemeContext);
 
@@ -21,7 +21,8 @@ export const Github = () => {
           🔰 Days I <span className="different"> Code</span>
         </h2>
         <Githubcalendar
-          username="duwesh"
+          username="Duwesh"
+          year={new Date().getFullYear()}
           blockSize={15}
           blockMargin={5}
           theme={themename}
