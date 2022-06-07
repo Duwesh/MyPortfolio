60c4d3bb011506c80ce12968ff13032a8111d425
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-06-07 10:57:58 +0530

    added masai cart project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -75,6 +75,7 @@ export const Projects = () => {
             </div>
           </div>
         </div>
+        {/* Apartment Manager  */}
         <div className="projects_container">
           <div className="project">
             <div className="project_videocontainer">
@@ -113,6 +114,55 @@ export const Projects = () => {
                 </a>
                 <a
                   href="https://github.com/Duwesh/frontend-apartment"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span type="button" className="btn btn--outline">
+                    Source Code
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Another Project  */}
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src="https://user-images.githubusercontent.com/67849097/172216672-4178dd9d-7c03-4444-b251-002d1b8c20e7.png"
+                  alt="Masai Cart"
+                />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>Masai Cart</h2>
+              <p>
+                It is a solo project executed in 3 day as a side project of mine
+                in Masai School. It has features like product searching and
+                sorting, filtering, Add to Cart and many more.
+              </p>
+              <div>
+                <SiReact />
+                <SiRedux />
+                <IoLogoJavascript />
+                <SiNodedotjs />
+                <SiExpress />
+                <SiMongodb />
+              </div>
+              <div>
+                <a
+                  href="https://shopping-cart-two-gray.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span type="button" className="btn btn--outline onbt">
+                    Deployment
+                  </span>
+                </a>
+                <a
+                  href="https://github.com/Duwesh/Masai_Cart"
                   target="_blank"
                   rel="noreferrer"
                 >
